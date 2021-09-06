7d3078ea82a05501d22ba7985524562c7d3d9639
Bryan Young Carvalho Soares <75449151+BryanYunche@users.noreply.github.com>
2021-09-05 21:26:04 -0300

    Gera uma PA e mostra todos os termos anteriores

--- gerador_de_PA.py
+++ gerador_de_PA.py
@@ -6,7 +6,7 @@ print(5*'-=-')
 print('teste de pa\n')
 a1 = int(input('Digite o primeiro termo da PA: '))
 r = int(input('Digite a razão da PA: '))
-an = int(input('Digite qual termo da PA você quer ver: '))
+an = int(input('Digite até qual termo da PA você quer ver: '))
 #c é a variavél de controle do segundo while
 c = 0
 mterm = 1
@@ -23,51 +23,3 @@ while mterm != 0:
   an = c + mterm
 print('{} termos foram vistos!'.format(an))
 print('Fim do Programa!')
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''sair = 'N'
-a1 = int(input('Digite o primeiro termo da PA: '))
-r  = int(input('Digite a razão da PA: '))
-an = int(input('Digite o número de termos que quer ver na tela: '))
-c1 = 1
-print('Os 10 primeiros termos da PA são: ')
-print(a1, end = ' ➙ ')
-while c1 < an:
-  pa = a1 + (c1*r)
-  c1 += 1
-  s = str(pa)
-  print(s, end = ' ➙ ')
-c2 = 1
-while sair == 'N':
-  sair = str(input('\nVocê ainda deseja continuar? [Y/N]: ')).upper
-  n = int(input('Quantos termos mais você quer ver? '))
-  while c2 < n:
-    pa = a1 + (c1*r)
-    c2 += 1
-    c1 += 1
-    s = str(pa)
-    print(s, end = ' ➙ ')
-print('FIM DO PROGAMA')'''
